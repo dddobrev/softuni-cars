ef89f4ee762d4ec4063c9d36141372a7ea54538f
luchob <lachezar.balev@gmail.com>
2021-01-03 09:38:55 +0200

    Skeleton of the project

---
+++ src/test/java/bg/softuni/cars/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
